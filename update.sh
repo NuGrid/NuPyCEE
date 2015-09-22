ca6a91ac4b0a67750ad7cc2891f87c50dc2512fe
Christian Ritter <critter@uvic.ca>
2015-09-22 13:45:24 -0700

    Sphinx update

--- update.sh
+++ update.sh
@@ -1,4 +1,10 @@
+echo 'Updating SPHINX documentation...'
+cd SPHINX
+update html
+echo 'done'
+echo 'Submitting to bitbucket'
 #inside the NUPYCEE dir
 git commit  -am 'Sphinx update'
 git push -u origin master
-echo 'Update of web page might take a moment'
+echo 'Done
+echo 'Note: Update of web pages might take a moment'
