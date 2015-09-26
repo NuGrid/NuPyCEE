bf28dfa223ef0f76f252e46b98f5081f1af04758
Christian Ritter <critter@uvic.ca>
2015-09-26 14:36:58 -0700

    Sphinx update

--- update.sh
+++ update.sh
@@ -1,8 +1,14 @@
+#sphinx doc, manually done with 'make html' in SPHINX dir
 echo 'Updating SPHINX documentation...'
 cd SPHINX
 make html
 cd ../
 echo 'Updating SPHINX documentation done'
+#for online access
+echo 'compile DOC pdf'
+DOC/runlatex
+echo 'compile DOC pdf done'
+# finally submit
 echo 'Submitting to bitbucket'
 #inside the NUPYCEE dir
 git commit  -am 'Sphinx update'
