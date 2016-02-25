7a7c69c2a19dc1cbcc14e8bed505a6859daf4202
critter1 <critter@uvic.ca>
2016-02-25 13:39:10 -0800

    New yield interpolation in __lin_int_yields and some other minux fixes

--- stellab.py
+++ stellab.py
@@ -405,7 +405,7 @@ class stellab():
     ##############################################
     #                Plot Spectro                #
     ##############################################
-    def plot_spectro(self, galaxy='', xaxis='[Fe/H]', yaxis='[Mg/Fe]', \
+    def plot_spectro(self,fig=0, galaxy='', xaxis='[Fe/H]', yaxis='[Mg/Fe]', \
                      fsize=[10,4.5], fontsize=14, rspace=0.6, bspace=0.15, \
                      labelsize=15, legend_fontsize=14, norm='', obs='',\
                      overplot=False, return_xy=False, show_err=False, \
@@ -479,6 +479,8 @@ class stellab():
 
             Example : x, y = stellab.plot_spectro(return_xy=True)
 
+	fig : figure ID
+
         fsize : 2D float array
 
 	     Figure dimension/size.
@@ -522,7 +524,7 @@ class stellab():
 
         # Show the frame of the plot
         if not overplot and not return_xy:
-            plt.figure(figsize=(fsize[0],fsize[1]))
+            plt.figure(fig,figsize=(fsize[0],fsize[1]))
 
         # If data need to be re-normalized ...
         re_norm = False
