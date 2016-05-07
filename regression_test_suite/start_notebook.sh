2f1fb0924ee50f8ce382e8478227e58eed9f4e01
critter1 <critter@uvic.ca>
2016-05-07 08:42:37 -0700

    added regression test suite

---
+++ regression_test_suite/start_notebook.sh
@@ -0,0 +1,15 @@
+#set the path to your NUPYCEE dir
+export SYGMADIR=../
+
+#export path
+export PYTHONPATH=$PYTHONPATH:$SYGMADIR
+
+#Set the links properly to use the feature of yield table creation and SPHINX documentation
+rm -f doc
+ln -s $SYGMADIR/SPHINX/build/html doc
+rm -f yield_tables
+ln -s $SYGMADIR/yield_tables/ yield_tables
+
+#starts notebook server
+jupyter-notebook
+
