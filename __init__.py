5c7f485838ffdf7c14e27639fb487f947f2da04b
Alexander Ji <alexji@mit.edu>
2018-07-30 11:36:05 -0400

    Added __init__

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from __future__ import (division, print_function, absolute_import,
+                        unicode_literals)
+
+from . import (sygma, omega, stellab, imf_input, read_yields, chem_evol)
