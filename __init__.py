ee79e02cbe3c6ff464492992adc6a2a76b124740
Benoit Cote <benoit.cote.4@ulaval.ca>
2019-07-12 15:05:03 -0400

    BC: re-format init

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from __future__ import (division, print_function, absolute_import,
-                        unicode_literals)
-
-from . import (sygma, omega, stellab, imf_input, read_yields, chem_evol)
