ae33958505f79a81e7393c2232100e4b6c21f110
Andrés Yagüe López <and.yague@gmail.com>
2020-04-21 14:47:53 +0200

    Interpolation routine adapted for 1-D and 2-D numpy arrays

--- chem_evol.py
+++ chem_evol.py
@@ -7605,15 +7605,33 @@ class chem_evol(object):
     ##############################################
     #          Interpolation routine             #
     ##############################################
-    def __interpolation(self, x_arr, y_arr, xx, indx):
+    def __interpolation(self, x_arr, y_arr, xx, indx, interp_list):
 
         '''
-        This function
+        This function interpolates with the Steffen 1990 algorithm, adding
+        linear extra points at both ends of the interval.
 
         Argument
         ========
+            x_arr: coordinate array
+            y_arr: 1-D or 2-D numpy array for interpolation
+            xx: value for which y_arr must be interpolated
+            indx: interpolation index such that
+                x_arr[indx] < xx < x_arr[indx + 1].
+                The minimum value is 0 and the maximum is len(x_arr) - 1.
+            interp_list: list holding the interpolation coefficients.
+                it should have the same size and dimensions as y_arr and
+                initialized to None.
 
         '''
+
+        # Get the dimensions and catch non-numpy arrays
+        try:
+            dimensions = y_arr.ndim
+        except AttributeError:
+            raise Exception("The interpolation routine uses numpy arrays")
+        except:
+            raise
 
         # Return if last extreme
         if indx == len(x_arr) - 1:
@@ -7624,10 +7642,18 @@ class chem_evol(object):
             raise Exception("Interpolating outside of range!")
 
         # Return the calculation with coefficients if exists
-        if self.interp_list[indx] is not None:
-            coefs = self.interp_list[indx]
-            deltx = xx - x_arr[indx]
-            return coefs[0]*deltx**3 + coefs[1]*deltx**2 + coefs[2]*deltx + y_arr[indx]
+        if dimensions == 1:
+            if interp_list[indx] is not None:
+                coefs = interp_list[indx]
+                deltx = xx - x_arr[indx]
+                return coefs[0]*deltx**3 + coefs[1]*deltx**2 + coefs[2]*deltx + y_arr[indx]
+        elif dimensions == 2:
+            if interp_list[indx][0] is not None:
+                coefs = interp_list[indx]
+                deltx = xx - x_arr[indx]
+                return coefs[0]*deltx**3 + coefs[1]*deltx**2 + coefs[2]*deltx + y_arr[indx]
+        else:
+            raise Exception("Current support for up to 2-d in interpolation method")
 
         # If not, we have to calculate the coefficients for this region
         x0 = x_arr[indx]; xp1 = x_arr[indx + 1]
@@ -7658,7 +7684,12 @@ class chem_evol(object):
 
         # Derivative
         deriv0 = np.sign(sim1) + np.sign(si0)
-        deriv0 = deriv0*min(abs(sim1), abs(si0), 0.5*abs(pi0))
+        if dimensions == 1:
+            deriv0 = deriv0*min(abs(sim1), abs(si0), 0.5*abs(pi0))
+        elif dimensions == 2:
+            deriv0 = deriv0*np.minimum(abs(sim1),\
+                    np.minimum(abs(si0), 0.5*abs(pi0)))
+
 
         # Calculate sip1, pip1 and derivp1
         if indx < len(x_arr) - 2:
@@ -7679,14 +7710,18 @@ class chem_evol(object):
 
         # Derivative
         derivp1 = np.sign(si0) + np.sign(sip1)
-        derivp1 = derivp1*min(abs(si0), abs(sip1), 0.5*abs(pip1))
+        if dimensions == 1:
+            derivp1 = derivp1*min(abs(si0), abs(sip1), 0.5*abs(pip1))
+        elif dimensions == 2:
+            derivp1 = derivp1*np.minimum(abs(si0), \
+                    np.minimum(abs(sip1), 0.5*abs(pip1)))
 
         # Now calculate coefficients (ci = deriv0; di = y0)
         ai = (deriv0 + derivp1 - 2*si0)/(hi0*hi0)
         bi = (3*si0 - 2*deriv0 - derivp1)/hi0
 
-        self.interp_list[indx] = (ai, bi, deriv0)
-        return self.__interpolation(x_arr, y_arr, xx, indx)
+        interp_list[indx] = (ai, bi, deriv0)
+        return self.__interpolation(x_arr, y_arr, xx, indx, interp_list)
 
 
     ##############################################
