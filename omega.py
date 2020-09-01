488af00c26e9f90cf587f28db99bc8a0cef6c554
Benoit Cote <benoitcote@MacBook-Pro-de-Benoit.local>
2020-08-31 21:13:26 -0400

    BC: test

--- omega.py
+++ omega.py
@@ -92,8 +92,13 @@ import os
 nupy_path = os.path.dirname(os.path.realpath(__file__))
 
 # Import NuPyCEE codes
-import NuPyCEE.sygma as sygma
-from NuPyCEE.chem_evol import *
+# BEFORE ChETEC SCHOOL
+#import NuPyCEE.sygma as sygma
+#from NuPyCEE.chem_evol import *
+
+import imp
+sygma = imp.load_source('sygma', nupy_path+'sygma.py')
+chem_evol = imp.load_source('chem_evol', nupy_path+'chem_evol.py')
 
 class omega( chem_evol ):
 
