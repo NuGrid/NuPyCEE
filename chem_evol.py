1c5dac4aee3c2581f20dadde293b2e35dacf3889
Benoit Cote <cotebenoit8@gmail.com>
2022-09-29 15:31:39 -0700

    BC: Added coma between "Rn" and "Fr" in elem_list_periodic_table.

--- chem_evol.py
+++ chem_evol.py
@@ -1560,9 +1560,10 @@ class chem_evol(object):
                 "Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te", "I", "Xe", "Cs", "Ba", "La", \
                 "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", \
                 "Lu", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", \
-                "Bi", "Po", "At", "Rn" "Fr", "Ra", "Ac", "Th", "Pa", "U", "Np", "Pu", \
+                "Bi", "Po", "At", "Rn", "Fr", "Ra", "Ac", "Th", "Pa", "U", "Np", "Pu", \
                 "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr", "Rf", "Db", "Sg", \
                 "Bh", "Hs", "Mt", "Ds", "Rg", "Cn", "Nh", "Fl", "Mc", "Lv", "Ts", "Og"]
+
 
     ##############################################
     #              Sort Isotope List             #
